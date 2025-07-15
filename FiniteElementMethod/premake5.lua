d769a2eff68b0a98fe1c5e31838cd116c2f40b0b
Dariusz Homa <dariuszhoma7@gmail.com>
2025-07-15 13:03:42 +0200

    feat: add precompiled header

--- FiniteElementMethod/premake5.lua
+++ FiniteElementMethod/premake5.lua
@@ -2,11 +2,30 @@ project "App"
   kind "ConsoleApp"
   language "C++"
   cppdialect "C++20"
+  staticruntime "off"
 
   targetdir (OutputDir .. "/%{prj.name}")
   objdir (IntermediateDir .. "/%{prj.name}")
+
+  pchheader "pch.h"
+  pchsource "src/pch.cpp"
 
   files {
     "src/**.h",
     "src/**.cpp"
   }
+
+  includedirs {
+    "src",
+  }
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    runtime "Debug"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    runtime "Release"
+    optimize "On"
+    symbols "On"
