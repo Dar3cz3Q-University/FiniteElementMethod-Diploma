606a83f14f288c2d48d962855532624ae6ffcd73
Dariusz Homa <dariuszhoma7@gmail.com>
2025-11-10 16:18:02 +0100

    fix: create import file for math module

---
+++ FiniteElementMethod/src/math/LinearAlgebra.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace fem
+{
+using Mat = Eigen::MatrixXd;
+using Vec = Eigen::VectorXd;
+
+using Mat2 = Eigen::Matrix2d;
+using Mat3 = Eigen::Matrix3d;
+using Mat4 = Eigen::Matrix4d;
+using Vec2 = Eigen::Vector2d;
+using Vec3 = Eigen::Vector3d;
+using Vec4 = Eigen::Vector4d;
+
+}
