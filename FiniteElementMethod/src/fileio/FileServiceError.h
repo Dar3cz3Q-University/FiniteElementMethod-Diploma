3a2eab66d211a21e75bd016c00639818c1cb4913
Dariusz Homa <dariuszhoma7@gmail.com>
2025-12-03 22:07:52 +0100

    feat: refactor ErrorToString in FileServiceError

--- FiniteElementMethod/src/fileio/FileServiceError.h
+++ FiniteElementMethod/src/fileio/FileServiceError.h
@@ -3,9 +3,10 @@
 #include <filesystem>
 #include <string>
 
-namespace fem::fileio {
+namespace fem::fileio
+{
 
-enum class FileServiceErrorCode
+enum class FileServiceErrorCode : int
 {
 	FileNotFound = 0,
 	ParentDirectoryNotFound,
@@ -21,34 +22,34 @@ struct FileServiceError
 	FileServiceErrorCode code;
 	std::filesystem::path path;
 	std::string message;
+
+	std::string ToString() const
+	{
+		using enum FileServiceErrorCode;
+
+		std::string msg = std::format("FileIOError[{}]: ", path.string());
+
+		switch (code)
+		{
+		case FileNotFound: msg += "File not found.\n"; break;
+		case ParentDirectoryNotFound: msg += "Parent directory not found.\n"; break;
+		case PermissionDenied: msg += "Permission denied.\n"; break;
+		case NotAFile: msg += "Path is not a regular file.\n"; break;
+		case ReadFailure: msg += "Failed to read file.\n"; break;
+		case WriteFailure: msg += "Failed to write file.\n"; break;
+		case Unknown: msg += "Unknown error.\n"; break;
+		}
+
+		if (!message.empty())
+		{
+			msg += "(";
+			msg += message;
+			msg += ")";
+		}
+
+		return msg;
+	}
 };
-
-inline std::string ErrorToString(const FileServiceError& err)
-{
-	using enum FileServiceErrorCode;
-
-	std::string msg = std::format("FileIOError[{}]: ", err.path.string());
-
-	switch (err.code)
-	{
-	case FileNotFound: msg += "File not found.\n"; break;
-	case ParentDirectoryNotFound: msg += "Parent directory not found.\n"; break;
-	case PermissionDenied: msg += "Permission denied.\n"; break;
-	case NotAFile: msg += "Path is not a regular file.\n"; break;
-	case ReadFailure: msg += "Failed to read file.\n"; break;
-	case WriteFailure: msg += "Failed to write file.\n"; break;
-	case Unknown: msg += "Unknown error.\n"; break;
-	}
-
-	if (!err.message.empty())
-	{
-		msg += "(";
-		msg += err.message;
-		msg += ")";
-	}
-
-	return msg;
-}
 
 }
 
@@ -57,6 +58,6 @@ struct std::formatter<fem::fileio::FileServiceError> : std::formatter<std::strin
 {
 	auto format(const fem::fileio::FileServiceError& err, std::format_context& ctx) const
 	{
-		return std::formatter<std::string_view>::format(fem::fileio::ErrorToString(err), ctx);
+		return std::formatter<std::string_view>::format(err.ToString(), ctx);
 	}
 };
